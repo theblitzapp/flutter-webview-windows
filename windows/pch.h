00a2698649e9b8a644a0973f41c6abcc81ad9a55
Trevin Miller <stumblinbear@gmail.com>
2026-03-06 17:41:58 -0600

    build: precompile headers for improved build times

---
+++ windows/pch.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// Windows SDK
+#include <inspectable.h>
+#include <roapi.h>
+#include <shlobj.h>
+#include <windows.h>
+
+// COM / WRL
+#include <wrl.h>
+
+// Direct3D
+#include <D3d11.h>
+#include <dxgi.h>
+
+// WinRT
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.System.h>
+#include <winrt/base.h>
+#include <winrt/windows.graphics.directx.direct3d11.h>
+
+// Windows UI / Graphics
+#include <DispatcherQueue.h>
+#include <windows.foundation.h>
+#include <windows.graphics.capture.h>
+#include <windows.ui.composition.desktop.h>
+#include <windows.ui.composition.h>
+#include <windows.ui.composition.interop.h>
+
+// WebView2
+#include <WebView2.h>
+#include <WebView2EnvironmentOptions.h>
+
+// WIL
+#include <wil/com.h>
